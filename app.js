9d6f61b536473d1193fe1397515a27985211fb9e
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-08-25 16:26:12 -0500

    app.js update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var index = require('./public/index');
-app.use('/', index);
+//var index = require('./public/index');
+//app.use('/', index);
 app.use(express.static(__dirname, './public'));
 
 var server = app.listen(process.env.PORT || 3000, function(){

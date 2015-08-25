4c7b5882745bd5d6c012e5c9ac9981ffc3772bd7
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-08-25 16:23:31 -0500

    update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var index = require('./public/index');
 app.use('/', index);
 app.use(express.static(__dirname, './public'));
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
     console.log('Listening on port: ', port);
 });
